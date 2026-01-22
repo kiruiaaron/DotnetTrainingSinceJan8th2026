0d63eea27f7e1e522db469fd88e6b0bda010a02a
kiruiaaron <kiruiaaron471@gmail.com>
2026-01-22 13:31:49 +0300

    Updated the blazor crud operation

--- BlazorWebApp/BlazorWebAppMovies/obj/Debug/net10.0/BlazorWebAppMovies.AssemblyInfo.cs
+++ BlazorWebApp/BlazorWebAppMovies/obj/Debug/net10.0/BlazorWebAppMovies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+326393eced4ed7cf1058ec73d0075ab2c195efe2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fcc6663f04cb01120a762513d5fc3a043ea54e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MyConsoleApp/MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+326393eced4ed7cf1058ec73d0075ab2c195efe2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fcc6663f04cb01120a762513d5fc3a043ea54e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
