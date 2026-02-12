186d303dd18f9401c7248b6fd5827a8a0cf76024
kiruiaaron <kiruiaaron471@gmail.com>
2026-02-12 16:52:12 +0300

    Sharing data in Blazor applications

--- BlazingPizzaSite/obj/Debug/net10.0/BlazingPizzaSite.AssemblyInfo.cs
+++ BlazingPizzaSite/obj/Debug/net10.0/BlazingPizzaSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+542a713b46b14bb16417ab7af02d32e4ecce73cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c026d3050b98f4a495bf9ba98da1b2a7e27a2f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizzaSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
